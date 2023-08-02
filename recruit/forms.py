728696e08603a805692de13ba8dbd0212e5729e5
akhmadakhmedov <ahmedov.thy@gmail.com>
2023-08-02 17:49:24 +0600

    get and post and class based view

---
+++ recruit/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Recruiter
+
+class RecruiterUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Recruiter
+        exclude = ['id']
--- recruit/views.py
+++ recruit/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Recruiter
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import RecruiterUpdateForm
+from django.urls import reverse_lazy
 
 
 def recruiter_list(request):
@@ -26,7 +29,44 @@ class RecruiterCreateView(CreateView):
     model = Recruiter
     fields = '__all__'
 
+class RecruiterUpdateView(View):
+    template = 'recruit/update.html'
+
+    def get_context(self, **kwargs):
+        id = kwargs["id"]
+        recruiter_object = Recruiter.objects.get(id=id)
+        form = RecruiterUpdateForm(instance=recruiter_object)
+        context = {}
+        context["form"] = form
+        context["recruiter"] = recruiter_object
+        return context
+
+    def my_render(self, request, context):
+        return render(request,
+                      self.template,
+                      context
+                      )
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context(**kwargs)
+        return self.my_render(request, context)
 
 
+    def post(self, request, *args, **kwargs):
+        context = self.get_context(**kwargs)
+        context["form"] = RecruiterUpdateForm(instance=context["recruiter"], data=request.POST)
+        form = context["form"]
+
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Info edited')
+        else:
+            messages.add_message(request, messages.WARNING, 'Failed')
+        return self.my_render(request, context)
 
 
+class RecruiterGenericUpdateView(UpdateView):
+    model = Recruiter
+    fields = [f.name for f in Recruiter._meta.get_fields() if f.name not in ['id', 'user']]
+    template_name = 'recruit/generic_update.html'
+    success_url = reverse_lazy('recruiter-list-class')
